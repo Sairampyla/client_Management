6a395917382ccca1e616f16527b196c9c39a568d
sairamp123 <sairamp@pllcsoftwaresolutions.com>
2021-08-03 16:03:00 +0530

    make it better

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(express.static(__dirname + 'dist/SampleProject'));
 
 app.get('/*', function(req,res) {
   // Replace the '/dist/<to_your_project_name>/index.html'
-  res.sendFile(path.join(__dirname+ 'dist/SampleProject/index.html'));
+  res.sendFile(path.join(__dirname,'dist/SampleProject/index.html'));
 });
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
